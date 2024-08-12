e643b04c4a01b46d4a2d65c8d5e283a23a034af4
zhangredli <zhangredli@163.com>
2024-08-12 13:41:44 +0800

    feat - 创建项目

---
+++ test/flutter_chinese_keyboard_test.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_chinese_keyboard/flutter_chinese_keyboard.dart';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+
+void main() {}
